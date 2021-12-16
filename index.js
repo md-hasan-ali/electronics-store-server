26aae03b87df99ab072d4daf69d3405236df18f0
md-hasan-ali <hasanaliringku@gmail.com>
2021-12-17 00:05:50 +0600

    get products for client side

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors');
 const app = express();
@@ -8,11 +9,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ayr4p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// run function create 
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("Eletronics-Store")
+        const productCollection = database.collection("Products");
 
 
+        // Get Api 
+        app.get('/products', async (req, res) => {
+            const result = await productCollection.find({}).toArray();
+            res.send(result);
+        });
 
-
-
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
