3d83285ca6729ef227649a580c1733c0db8ff1aa
md-hasan-ali <hasanaliringku@gmail.com>
2021-12-17 12:29:22 +0600

    place order and get single product details

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config()
 const cors = require('cors');
 const app = express();
@@ -10,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ayr4p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // run function create 
@@ -26,6 +26,14 @@ async function run() {
             const result = await productCollection.find({}).toArray();
             res.send(result);
         });
+
+        // Get Single Products
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await productCollection.find(query).toArray();
+            res.send(result);
+        })
 
     }
     finally {
